5c5faee09baf6e90021e49cad19a3e3c7aac5aee
Benjamin Tong <bentong95923@github.com>
2019-08-15 17:50:04 +1200

    test database connection

--- main.cpp
+++ main.cpp
@@ -9,6 +9,17 @@ using namespace std;
 
 int main()
 {
-    sqlite3 **ppDB;
-    sqlite3_open("data.db",ppDB);
+    sqlite3 *db;
+    int rc = sqlite3_open("data.db", &db);
+
+    if (rc)
+    {
+        fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+        return (0);
+    }
+    else
+    {
+        fprintf(stderr, "Opened database successfully\n");
+    }
+    sqlite3_close(db);
 }
